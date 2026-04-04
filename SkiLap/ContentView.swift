f39fbe2958c49bf53ab8a6db813dd18c3dbaa0ef
bowen <chromebookmarksbackup@gmail.com>
2026-04-04 21:37:35 +0900

    Initial Commit

---
+++ SkiLap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkiLap
+//
+//  Created by 賈博文 on 2026/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkiLap/SkiLapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkiLapApp.swift
+//  SkiLap
+//
+//  Created by 賈博文 on 2026/04/04.
+//
+
+import SwiftUI
+
+@main
+struct SkiLapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
